7bc51b877ad7fb37be4a90658d6a4eb1bda5ea5c
David Krzikalla <47105493+DavidKrzikalla@users.noreply.github.com>
2019-02-28 19:28:30 +0200

    -Comments edited -Tolerance now utilizes eps function

--- Assignment2/exercise3_6/test_trapeziodal2.m
+++ Assignment2/exercise3_6/test_trapeziodal2.m
@@ -9,7 +9,7 @@ F=@(x)(3.*x.^2-4.*x); %funtion anti-derivative
 
 expected=F(b)-F(a);
 I=trapezoidal(f, a, b, n);
-tol=1e-14; %original precision
+tol=eps*1e2; %original precision
 diff=abs(expected-I);
 assert(diff<=tol, 'Integration error larger than tolerance. Error: %g, Tol: %g',diff, tol);
 %% Test of trapezoidal for linear function with big numbers
@@ -22,18 +22,6 @@ F=@(x)(3e8.*x.^2-4e6.*x); %funtion anti-derivative
 
 expected=F(b)-F(a);
 I=trapezoidal(f, a, b, n);
-tol=1e-6; %precision lowered due to big numbers 
+tol=eps*1e10; %precision lowered due to big numbers 
 diff=abs(expected-I);
 assert(diff<=tol, 'Integration error larger than tolerance. Error: %g, Tol: %g',diff, tol);
-
-%Conclusion
-
-%Changing the original function to the function with high numbers cause the
-%test to fail. The fail of the test is caused the most likely by tolerance
-%setting to be very tight for such big numbers. The solution is that the
-%tolerance is set not so tight, say 1e-6. Then the test is completed
-%succesfully. Because the aim of this test was to check prescribed matlab
-%function for integration, using linear function there is no dependence on
-%division number and the results should be calculated exactly.
-%Since the tests were passed the matlab function could be considered as
-%right.
