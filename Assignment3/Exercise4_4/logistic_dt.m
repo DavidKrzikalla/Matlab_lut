74c3a4642fe2e230cda31a39af8cb3854251eabf
David Krzikalla <47105493+DavidKrzikalla@users.noreply.github.com>
2019-03-04 17:31:11 +0200

    Script accomplished: -adding while loop -printing plots
    Commenting

--- Assignment3/Exercise4_4/logistic_dt.m
+++ Assignment3/Exercise4_4/logistic_dt.m
@@ -1,16 +1,52 @@
-dt=25; % time step 
-T=60; % end step
+%% Exercise 4.4
+% This script solve numericaly ODE (Forward Euler method) within given timespan using iterative halving of time step.
 
-k=1:3; %halving coefficient
-dtk=2.^(-k)*dt; % halving of time step
+%Input variables:
+% dt - initial time step 
+% T - end time
+% U_0 - initial condition
 
-U_0=100; % initial condition
+%Input variables depending on the ODE:
+% f - time function
+% r - Growth function coefficient
+% M - Fixing growth function coefficient
+
+clc
+
+dt=input('Insert initial time step\n'); % initial time step 
+T=input('Insert end time\n'); % end time step
+U_0=input('Insert initial condition\n'); % initial condition
+
 r=0.1; %parameter of the growth function
 M=600; %parameter of the growth function
 f=@(t,u)r*(1-(u/M))*u; % growth function
-for i=k
-    [t, u] = ode_FE(f, dtk(i), T, U_0); %solving of ode
-    plot(t, u);
-    xlabel('t'); ylabel('N(t)');
-    hold on
+
+k=0; %halving coefficient
+
+dtk=2^(-k)*dt; % halving of time step    
+[t, u] = ode_FE(f, dtk, T, U_0); %solving of ode
+plot(t, u,'-b');
+hold on
+
+k=1;
+while true
+    dtk=2^(-k)*dt; % halving of time step
+    fprintf('New time step: %.4f\n', dtk)
+    
+    [t, u] = ode_FE(f, dtk, T, U_0); %solving of ode
+    plot(t, u,'-r');
+    legend('Last step','New step');
+    hold off
+    
+    decision=input('To continue, insert 1 \nTo exit insert 0 \n');
+    if decision~=1
+        break
+    else 
+        k=k+1;
+        t_last=t;
+        u_last=u;
+        plot(t_last, u_last,'-b');
+        hold on
+        continue
+    end
 end
